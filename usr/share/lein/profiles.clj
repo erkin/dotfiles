12aee7fb03520c51a390ab8dc9a602a6e43aef4a
erkin <erkinbatu@gmail.com>
2018-11-14 08:27:42 +0300

    other stuff

--- usr/share/lein/profiles.clj
+++ usr/share/lein/profiles.clj
@@ -1,4 +1,6 @@
-;; ugh
-{:user {:local-repo "/home/erkin/usr/share/m2/repository"
-        :repositories {"local" {:url "file:///home/erkin/usr/share/m2/repository"
-                                :releases {:checksum :ignore}}}}}
+{:user {:plugins [[jonase/eastwood "0.3.3"] [lein-bikeshed "0.5.1"]]
+;        :local-repo "/home/erkin/usr/share/m2/repository"
+;        :repositories {"local" {:url "file:///home/erkin/usr/share/m2/repository"
+;                                :releases {:checksum :ignore}}}
+}}
+{:repl {:plugins [[cider/cider-nrepl "0.18"]]}}
