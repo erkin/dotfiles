09053bfe225cbc146d387832c245032184ccd03d
erkin <erkinbatu@gmail.com>
2019-04-19 12:59:30 +0300

    Add proper nrepl for Leiningen

--- usr/share/lein/profiles.clj
+++ usr/share/lein/profiles.clj
@@ -1,6 +1,7 @@
-{:user {:plugins [[jonase/eastwood "0.3.3"] [lein-bikeshed "0.5.1"]]
-;        :local-repo "/home/erkin/usr/share/m2/repository"
-;        :repositories {"local" {:url "file:///home/erkin/usr/share/m2/repository"
-;                                :releases {:checksum :ignore}}}
-}}
-{:repl {:plugins [[cider/cider-nrepl "0.18"]]}}
+{:user {:plugins [[jonase/eastwood "0.3.3"]
+                  [lein-bikeshed "0.5.2"]
+                  [cider/cider-nrepl "0.21.1"]
+                  [nrepl/lein-nrepl "0.3.2"
+                   :exclusions [org.clojure/tools.logging
+                                commons-codec
+                                commons-io]]]}}
