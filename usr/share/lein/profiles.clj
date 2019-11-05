31f8f9171916188ad4fb714cc42952ad600f6267
erkin <me@erkin.party>
2019-11-05 20:56:07 +0300

    Update Lein config

--- usr/share/lein/profiles.clj
+++ usr/share/lein/profiles.clj
@@ -1,12 +1,7 @@
 {:user {:plugins [[jonase/eastwood "0.3.5"]
                   [lein-bikeshed "0.5.2"]
-                  [lein-kibit "0.1.6"
-                   :exclusions [org.clojure/tools.namespace]]
-                  [cider/cider-nrepl "0.22.0-beta1"]
-                  [nrepl/lein-nrepl "0.3.2"
-                   :exclusions [commons-codec
-                                commons-io
-                                org.clojure/tools.logging]]
-                  [venantius/yagni "0.1.7"
-                   :exclusions [org.clojure/clojure
-                                org.clojure/tools.namespace]]]}}
+                  [lein-kibit "0.1.7"]
+                  [cider/cider-nrepl "0.22.0-beta3"]
+                  [nrepl/lein-nrepl "0.3.2"]
+                  [venantius/yagni "0.1.7"]]
+        :jvm-opts ["--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED"]}}
